3e31aea62eb1a6076f3b21c522d47a26c5119646
LeTienQuyet <21520428@gm.uit.edu.vn>
2025-02-09 18:15:53 +0700

    prepare data for training

--- src/train.py
+++ src/train.py
@@ -1,8 +1,47 @@
 from facenet_pytorch import InceptionResnetV1
 from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+from torchvision.datasets import LFWPairs
+from tqdm import tqdm
 
+import argparse
+import torch
 import torchvision.transforms as transforms
 import torch.nn as nn
+import torch.optim as optim
+
+import pathlib
+pathlib.PosixPath = pathlib.WindowsPath
+
+class CustomDataset(Dataset):
+    def __init__(self, data, transform=None, image_size=(160, 160)):
+        self.data = data
+
+        if transform is None:
+            transform = transforms.Compose([
+                transforms.Resize(image_size),
+                transforms.ToTensor()
+            ])
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        img1, img2, label = self.data[idx]
+        img1, img2 = self.transform(img1), self.transform(img2)
+        return img1, img2, label
+
+def prepare_data(transform, batch_size=32, path_save_data="../data/"):
+    train_data = LFWPairs(root=path_save_data, split="train", image_set="original", download=True)
+    test_data = LFWPairs(root=path_save_data, split="test", image_set="original", download=True)
+
+    train_dataset = CustomDataset(train_data, transform=transform)
+    test_dataset = CustomDataset(test_data, transform=transform)
+
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    return train_dataloader, test_dataloader
 
 def face_detection(img, detector):
     results = detector(img)
@@ -14,7 +53,6 @@ def face_detection(img, detector):
     best_prediction = predictions.loc[predictions["confidence"].idxmax()]
     x_min, y_min, x_max, y_max = int(best_prediction["xmin"]), int(best_prediction["ymin"]), int(best_prediction["xmax"]), int(best_prediction["ymax"])
     return img.crop((x_min, y_min, x_max, y_max))
-
 
 class FaceDectionTransform:
     def __init__(self, detector):
@@ -49,3 +87,20 @@ class SiameseNeuralNetwork(nn.Module):
         x = self.fc(x)
         return x
 
+def main(batch_size, detector_ckpt):
+    detector = torch.hub.load("ultralytics/yolov5", "custom", path=detector_ckpt, force_reload=True, verbose=False)
+    transform = transform_image(detector)
+    train_dataloader, test_dataloader = prepare_data(transform)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Hyper-parameters for training")
+
+    parser.add_argument("--batch_size", type=int, help="Batch size", default=32)
+    parser.add_argument("--detector_ckpt", type=str, help="Checkpoint of detector", default="../checkpoint/yolov5n/best.pt")
+
+    args = parser.parse_args()
+
+    main(
+        batch_size=args.batch_size,
+        detector_ckpt=args.detector_ckpt
+    )
